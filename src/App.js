18f99cf5a9caa319ce26dedc883a2593a45e06d0
SamiraKarimi <samira.ka006@gmail.com>
2020-01-02 07:55:27 -0600

    storing user data in the app

--- src/App.js
+++ src/App.js
@@ -16,13 +16,28 @@ class App extends React.Component {
     };
   }
   unsubscribeFromAuth = null;
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
 
-      console.log(user);
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
+  
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
